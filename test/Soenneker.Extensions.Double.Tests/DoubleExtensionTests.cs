0b2c79c9d238b0c072fc1c310f8f7cc5e42669b4
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:19 +0000

    Tests

---
+++ test/Soenneker.Extensions.Double.Tests/DoubleExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Double.Tests;
+
+public class DoubleExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
